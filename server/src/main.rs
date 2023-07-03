f7c8ede0c2e3f8f6e2ff31f174137a53bfeb63ff
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-07-03 23:16:07 +0900

    update integration tests

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+use rocket::{fairing::{Fairing, Info, Kind}, http::Header, Request, Response};
+
 #[macro_use]
 extern crate rocket;
 
@@ -7,10 +9,31 @@ mod tests;
 mod path;
 mod quote;
 
+pub struct CORS;
+
 #[get("/")]
 pub fn health_check() -> &'static str {
     "OK"
 }
+
+// ref: https://stackoverflow.com/a/64904947/13743156
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 
 #[launch]
 fn rocket() -> _ {
@@ -18,4 +41,5 @@ fn rocket() -> _ {
         .mount("/healthcheck", routes![health_check])
         .mount("/path", routes![path::get_path])
         .mount("/quote", routes![quote::quote_prices])
+        .attach(CORS)
 }
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import {
+  Container,
+  Flex,
+  Box,
+  Text,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import axios from 'axios';
 
 function App() {
+  const [tokenIn, setTokenIn] = React.useState('');
+  const [tokenOut, setTokenOut] = React.useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      axios.get(`http://localhost:8000/path/${tokenIn}/${tokenOut}`, {})
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(res.data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    };
+    fetchData();
+
+  }, [tokenIn, tokenOut]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+        <Box
+          bg="#02054B"
+          color="white"
+          borderRadius="lg"
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }}>
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems="flex-start">
+                      <Text
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        color="#DCE2FF"
+                      >
+                      </Text>
+                    </VStack>
+                  </Box>
+                  <HStack
+                    mt={{ lg: 10, md: 10 }}
+                    spacing={5}
+                    px={5}
+                    alignItems="flex-start">
+                  </HStack>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>token in</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                          />
+                          <Input type="text" size="md"
+                            onChange={(e) => setTokenIn(e.target.value)}
+                          />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>token out</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement
+                            pointerEvents="none"
+                          />
+                          <Input type="text" size="md"
+                            onChange={(e) => setTokenOut(e.target.value)}
+                          />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button
+                          variant="solid"
+                          bg="#0D74FF"
+                          color="white"
+                          _hover={{}}>
+                          Quote
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
   );
 }
 
